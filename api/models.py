a1036d70ae98318f844a33cbf3d62e9e14b53f8b
Alban PRATS <alban.prats@insa-lyon.fr>
2017-10-09 11:45:46 +0200

    added model event

--- api/models.py
+++ api/models.py
@@ -64,3 +64,8 @@ class PricingRule(models.Model):
     value = models.IntegerField()
     pricings = models.ManyToManyField(Pricing, related_name='rules')
 
+
+class Event(models.Model):
+    eventName = models.CharField(max_length=30)
+    invitRequired = models.BooleanField()
+    eventGauge = models.IntegerField()
