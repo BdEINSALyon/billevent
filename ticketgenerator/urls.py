c7d9b3013b53277739839bc260b04b547d5a344e
flalleve <francois.lalleve@laposte.net>
2017-10-10 18:29:33 +0200

    Bugged code with image. Don't find file url syntax.

--- ticketgenerator/urls.py
+++ ticketgenerator/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.some_view),
+    url(r'^$', views.generate_ticket),
 ]
--- ticketgenerator/views.py
+++ ticketgenerator/views.py
@@ -1,23 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
-from io import BytesIO
+from reportlab.lib.pagesizes import A4
 from reportlab.pdfgen import canvas
 from django.http import HttpResponse
 
-def some_view(request):
+def generate_ticket(request):
     # Create the HttpResponse object with the appropriate PDF headers.
     response = HttpResponse(content_type='application/pdf')
-    response['Content-Disposition'] = 'attachment; filename="somefilename.pdf"'
+    response['Content-Disposition'] = 'attachment; filename="ticket.pdf"'
 
-    buffer = BytesIO()
-
-    # Create the PDF object, using the BytesIO object as its "file."
-    p = canvas.Canvas(buffer)
+    # Create the PDF object, using the response object as its "file."
+    p = canvas.Canvas(response, pagesize=A4)
 
     # Draw things on the PDF. Here's where the PDF generation happens.
     # See the ReportLab documentation for the full list of functionality.
-    p.drawString(100, 100, "Hello world.")
+    p.drawString(50, 800, "Hello world. ")
+    p.drawImage("ticketgenerator/views.py", 0,0, width=None,height=None,mask=None)
+
 
     # Close the PDF object cleanly.
     p.showPage()
