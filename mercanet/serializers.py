b613e856deaf64a303747ee527d4c5fe3f41548d
JeanRibes <jean@Sentinelle>
2017-10-12 14:35:32 +0200

    modifié pour coller aux champs Mercanet

--- mercanet/serializers.py
+++ mercanet/serializers.py
@@ -1,11 +1,14 @@
 from rest_framework import serializers
 from models import TransactionMercanet
-import sealTransaction
 class TransactionMercanetSerializer(serializers.Serializer):
-    idTransaction = serializers.IntegerField(required = True)
-    idTransaction = serializers.IntegerField(required = True)
-    transactionTS = serializers.BooleanField(required = True)
-    transactionState = serializers.CharField(max_length = 100)
-    paymentMethod = serializers.BooleanField(required = False) # ce sera toujours en CB, non ?
-    idOrder = Serializer.IntegerField(required = True)
+    amount = serializers.IntegerField(required = True)
+    currencyCode = serializers.IntegerField(require = True)
+    interfaceVersion = serializers.CharField(min_length = 1)
+    keyVersion = serializers.IntegerField()
+    merchantId = serializers.CharField()
+    normalReturnUrl = serializers.URLField()
+    orderChannel = serializers.CharField()
+    transactionReference = serializers.IntegerField()
+    Seal = serializers.CharField(min_length = 1)
+
 # comment peut-on ajouter le seal à ce serializer ? il faut que ce field (qui représente la requête à Mercanet), contienne le hash du seal
