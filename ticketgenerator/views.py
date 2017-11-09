ca5d103a7171e2f919685d9541791d9bca77377c
Nornort <francois.lalleve@laposte.net>
2017-11-09 14:36:11 +0100

    Barcode

--- ticketgenerator/views.py
+++ ticketgenerator/views.py
@@ -5,10 +5,12 @@ from reportlab.lib.pagesizes import A4, landscape
 from reportlab.pdfgen import canvas
 from django.http import HttpResponse
 from reportlab.lib.units import mm
+from reportlab.graphics.barcode import eanbc, qr, usps
 from reportlab.graphics.shapes import Drawing
+from reportlab.graphics import renderPDF
 
 def generate_ticket(request):
-    uid = "MAO0RTFM"
+    uid = "12345678"
     order = "NEOMATRIX"
     holder_last_name = "SKYWALKER"
     holder_first_name = "Anakin"
@@ -33,29 +35,44 @@ def generate_ticket(request):
 
     # Draw things on the PDF. Here's where the PDF generation happens.
     # See the ReportLab documentation for the full list of functionality.
-    qr_x = 85.60
-    qr_y = 53.98
+    barcode = eanbc.Ean13BarcodeWidget(uid)
+    ba_bounds = barcode.getBounds()
+    ba_width = ba_bounds[2] - ba_bounds[0]
+    ba_height = ba_bounds[3] - ba_bounds[1]
+    qr_code = qr.QrCodeWidget(uid)
+    qr_bounds = qr_code.getBounds()
+    qr_width = qr_bounds[2] - qr_bounds[0]
+    qr_height = qr_bounds[3] - qr_bounds[1]
+    qr_x = ba_width + qr_width
+    qr_y = ba_height + qr_height
     p.drawImage("ticketgenerator/background.png", 0 * mm, 0 * mm, width=373*mm, height=210*mm, mask=None)
     p.setFont("Helvetica", 12)
-    p.setStrokeColorRGB(255,255,255)
+    p.setStrokeColorRGB(255, 255,255)
     p.setFillColorRGB(255, 255, 255)
-    p.drawString(20 * mm, A4[0] - (20 + qr_y + 10) * mm, "{}".format(holder_last_name))
-    p.drawString(20 * mm, A4[0] - (20 + qr_y + 20) * mm, "{}".format(holder_first_name))
-    p.drawString(20 * mm, A4[0] - (20 + qr_y + 30) * mm, "Statut : {}".format(status))
-    p.drawString(20 * mm, A4[0] - (20 + qr_y + 40) * mm, "Prix TTC : {} €".format(price))
-    p.drawString(20 * mm, A4[0] - (20 + qr_y + 50) * mm, "Date d'émission : {}".format(issuing_date))
+    p.rect(20 * mm, A4[0] - 20 * mm - qr_y, qr_x - qr_width, qr_y, stroke=0, fill=1)
+    d = Drawing(50, 10)
+    d.add(barcode)
+    renderPDF.draw(d, p, 20 * mm, A4[0] - 20 * mm - ba_height)
+    d = Drawing(45, 45)
+    d.add(qr_code)
+    renderPDF.draw(d, p, 20 * mm, A4[0] - 20 * mm - qr_y)
+    p.drawString(20 * mm, A4[0] - qr_y - (20 + 10) * mm, "{}".format(holder_last_name))
+    p.drawString(20 * mm, A4[0] - qr_y - (20 + 20) * mm, "{}".format(holder_first_name))
+    p.drawString(20 * mm, A4[0] - qr_y - (20 + 30) * mm, "Statut : {}".format(status))
+    p.drawString(20 * mm, A4[0] - qr_y - (20 + 40) * mm, "Prix TTC : {} €".format(price))
+    p.drawString(20 * mm, A4[0] - qr_y - (20 + 50) * mm, "Date d'émission : {}".format(issuing_date))
     p.setFont("Helvetica-Bold", 25)
-    p.drawString((20 + qr_x + 5) * mm, A4[0] - (20 + 10) * mm, "Gala INSA Lyon 2018")
-    p.drawString((20 + qr_x + 5) * mm, A4[0] - (20 + 10 + 15) * mm, "9 et 10 février")
-    p.drawString((20 + qr_x + 5) * mm, A4[0] - (20 + 10 + 30) * mm, "La sucrière")
+    p.drawString(qr_x + (20 + 5) * mm, A4[0] - (20 + 10) * mm, "Gala INSA Lyon 2018")
+    p.drawString(qr_x + (20 + 5) * mm, A4[0] - (20 + 10 + 15) * mm, "9 et 10 février")
+    p.drawString(qr_x + (20 + 5) * mm, A4[0] - (20 + 10 + 30) * mm, "La sucrière")
     p.setFont("Helvetica", 12)
-    p.drawString((20 + qr_x + 5) * mm, A4[0] - (20 + 10 + 40) * mm, "49-50 Quai Rambaud 69002 Lyon - France")
-    p.drawString((20 + qr_x + 5) * mm, A4[0] - (20 + qr_y + 10) * mm, "Numéro : {}".format(uid))
-    p.drawString((20 + qr_x + 5) * mm, A4[0] - (20 + qr_y + 20) * mm, "Organisateur : BdE INSA Lyon")
-    p.drawString((20 + qr_x + 5) * mm, A4[0] - (20 + qr_y + 30) * mm, "Acheteur : {} {}".format(buyer_last_name, buyer_first_name))
-    p.drawString((20 + qr_x + 5) * mm, A4[0] - (20 + qr_y + 40) * mm, "Date de commande : {}".format(order_date))
-    p.drawString((20 + qr_x + 5) * mm, A4[0] - (20 + qr_y + 50) * mm, "Numéro de commande : {}".format(order))
-    p.drawString((20 + qr_x + 5) * mm, A4[0] - (20 + qr_y + 60) * mm, "Valable pour : 1 personne")
+    p.drawString(qr_x + (20 + 5) * mm, A4[0] - (20 + 10 + 40) * mm, "49-50 Quai Rambaud 69002 Lyon - France")
+    p.drawString(qr_x + (20 + 5) * mm, A4[0] - qr_y - (20 + 10) * mm, "Numéro : {}".format(uid))
+    p.drawString(qr_x + (20 + 5) * mm, A4[0] - qr_y - (20 + 20) * mm, "Organisateur : BdE INSA Lyon")
+    p.drawString(qr_x + (20 + 5) * mm, A4[0] - qr_y - (20 + 30) * mm, "Acheteur : {} {}".format(buyer_last_name, buyer_first_name))
+    p.drawString(qr_x + (20 + 5) * mm, A4[0] - qr_y - (20 + 40) * mm, "Date de commande : {}".format(order_date))
+    p.drawString(qr_x + (20 + 5) * mm, A4[0] - qr_y - (20 + 50) * mm, "Numéro de commande : {}".format(order))
+    p.drawString(qr_x + (20 + 5) * mm, A4[0] - qr_y - (20 + 60) * mm, "Valable pour : 1 personne")
     p.drawImage("ticketgenerator/bde.png", A4[1] - (20 + 22.53) * mm, A4[0] - (20 + 30) * mm, width=22.53 * mm,
                 height=30 * mm, mask=None)
     p.drawString(90 * mm, 20 * mm,
