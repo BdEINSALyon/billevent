1f19a09970a2c13949cf6e4f82ae700bd9c2f565
Alban PRATS <alban.prats@insa-lyon.fr>
2017-11-13 22:12:35 +0100

    Added swagger

--- billetterie/urls.py
+++ billetterie/urls.py
@@ -15,8 +15,18 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from rest_framework_swagger.views import get_swagger_view
+
+schema_view = get_swagger_view(title='Billevent API')
+
 
 urlpatterns = [
     url(r'^', include('api.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^swagger/', schema_view),
+    url(r'^admin/', admin.site.urls),
+    url(r'^ticketgenerator/', include('ticketgenerator.urls')),
+    url(r'^', include('frontend.urls')),
+    url(r'^', include('api.urls')),
 ]
+
