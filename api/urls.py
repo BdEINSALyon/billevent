286c40575cdf7f9947bd1f242f552f9224b93f25
Hugo Reymond <hugo.reymond@bde-insa-lyon.fr>
2017-11-14 17:52:11 +0100

    Ajout: Possibilité de récupérer les options par id et par produit

--- api/urls.py
+++ api/urls.py
@@ -11,8 +11,18 @@ router.register(r'events', views.EventsViewSet)
 urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^', include('rest_framework.urls', namespace='rest_framework')),
+
+    #Products Endpoints
     url(r'products/list/(?P<event>[0-9]+)',views.products_list, name="product-list"),
-    url(r'products/(?P<id>[0-9]+)',views.products_by_id,name="product-by-id"),
-    url(r'options/list_by_product/(?P<product_id>[0-9]+)',views.option_by_product,name="option-by-product"),
-    url(r'products/can_buy_one_more/(?P<product_id>[0-9]+)',views.canBuyOneMore,name="can-buy-one"),
+    url(r'products/(?P<id>[0-9]+)$',views.products_by_id,name="product-by-id"),
+    url(r'products/(?P<product_id>[0-9]+)/canBuyOneMore',views.canBuyOneMoreProduct,name="can-buy-one"),
+
+    #Options EndPoints
+    url(r'options/(?P<id>[0-9]+)$', views.option_by_id, name="product-by-id"),
+    url(r'options/(?P<option_id>[0-9]+)/canBuyOneMore', views.canBuyOneMoreOption, name="can-buy-one"),
+
+    #url(r'options/question/')
+    url(r'options/byProduct/(?P<product_id>[0-9]+)', views.option_by_product, name="option-by-product"),
+
 ]
+
--- api/views.py
+++ api/views.py
@@ -4,8 +4,9 @@ from rest_framework import viewsets, status
 from rest_framework.decorators import detail_route, api_view
 from rest_framework.response import Response
 
-from api.models import Event, Order, Option, Product, Billet, PricingRule
-from .serializers import UserSerializer, GroupSerializer, EventSerializer, OrderSerializer, OptionSerializer
+from api.models import Event, Order, Option, Product, Billet, PricingRule, Question
+from .serializers import UserSerializer, GroupSerializer, EventSerializer, OrderSerializer, OptionSerializer, \
+    ProductSerializer
 
 
 class EventsViewSet(viewsets.ReadOnlyModelViewSet):
@@ -65,7 +66,24 @@ def products_by_id(request, id):
         return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == "GET":
+        return Response(ProductSerializer(product).data)
+
+@api_view(['GET'])
+def option_by_id(request, id):
+    """
+
+    :param request:
+    :param id: L'ID du produit que l'on souhaite avoir
+    :return: 404 ou le produit demandé
+    """
+    try:
+        product = Option.objects.filter(id=id);
+    except Option.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == "GET":
         return Response(OptionSerializer(product).data)
+
 
 @api_view(['GET'])
 def option_by_product(request, product_id):
@@ -85,13 +103,13 @@ def option_by_product(request, product_id):
         return Response(OptionSerializer(options,many="true").data)
 
 @api_view(['GET'])
-def canBuyOneMore(request, product_id):
+def canBuyOneMoreProduct(request, product_id):
     """
     Allow the user to know if there are still availables billets
 
     :param request:
     :param id: The id of the product
-    :return: Le nombre de billets restants, 999999 si il n'existe pas de limite, -999 si une erreur est détectée
+    :return: True si il reste des places, ou si il n'existe pas de quotas, false si il n'y a pas de places ou si le programme renvoie une erreur
     """
 
 
@@ -100,7 +118,45 @@ def canBuyOneMore(request, product_id):
         nombreBillet = Billet.objects.filter(product=product_id).count()
 
     except Product.DoesNotExist:
-        return Response(999999)
+        return Response(False)
     except Billet.DoesNotExist:
-        return Response(999999)
-    return  Response(BilletsMax-nombreBillet)
+        return Response(True)
+    if BilletsMax-nombreBillet>0:
+        return True
+    else:
+        return False
+@api_view(['GET'])
+def canBuyOneMoreOption(request, option_id):
+    """
+    Allow the user to know if there are still availables billets
+
+    :param request:
+    :param id: The id of the product
+    :return: True si il reste des places, ou si il n'existe pas de quotas, false si il n'y a pas de places ou si le programme renvoie une erreur
+    """
+
+
+    try:
+        BilletsMax = Option.objects.get(id=option_id).rules.get(type="T").value
+        nombreBillet = Billet.objects.filter(option=option_id).count()
+
+    except Product.DoesNotExist:
+        return Response(False)
+    except Billet.DoesNotExist:
+        return Response(True)
+    if BilletsMax-nombreBillet>0:
+        return True
+    else:
+        return False
+
+@api_view(['GET'])
+def question_by_Product(request,id_product):
+    """
+    Permet de récupérer les question liées aux produits
+
+    :param request:
+    :param id_product: Le produit
+    :return: La liste JSON des questions liées
+    """
+
+    return Response(Question.objects.filter(product=id_product).data)
