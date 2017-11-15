e8f5755f329cb4896cf1ecf0cb5452ca9a5df601
Jean Ribes <jean.christophe.ribes@sfr.fr>
2017-11-15 21:12:10 +0100

    rajouté le seal à la sérialisation

--- mercanet/serializers.py
+++ mercanet/serializers.py
@@ -1,8 +1,12 @@
 from rest_framework import serializers
 from models import TransactionMercanet
+from mercanet import sealTransaction
+secretKey = os.environ['MERCANET_SECRET_KEY']
 class TransactionMercanetSerializer(serializers.Serializer):
     class Meta:
         model = TransactionMercanet
         fields = ('amount', 'currencyCode', 'interfaceVersion', 'keyVersion', 'merchantID', 'normalReturnUrl', 'transactionReference', 'Seal')
-
+	def create(self):
+	    TransactionMercanet.Seal = sealTransaction(TransactionMercanet.amount, TransactionMercanet.currencyCode, TransactionMercanet.interfaceVersion, TransactionMercanet.keyVersion, TransactionMercanet.merchantId, TransactionMercanet.normalReturnUrl, TransactionMercanet.orderChannel, TransactionMercanet.transactionReference, secretKey)
+	    return 
 # comment peut-on ajouter le seal à ce serializer ? il faut que ce field (qui représente la requête à Mercanet), contienne le hash du seal
