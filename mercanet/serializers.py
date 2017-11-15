0cd6eb1d72112355e6d656b3534ae999e10cc1e8
Jean Ribes <jean.christophe.ribes@sfr.fr>
2017-11-15 20:34:39 +0100

    c'est plus simple
    utilisation des serializers automatiques

--- mercanet/serializers.py
+++ mercanet/serializers.py
@@ -1,14 +1,8 @@
 from rest_framework import serializers
 from models import TransactionMercanet
 class TransactionMercanetSerializer(serializers.Serializer):
-    amount = serializers.IntegerField(required = True)
-    currencyCode = serializers.IntegerField(require = True)
-    interfaceVersion = serializers.CharField(min_length = 1)
-    keyVersion = serializers.IntegerField()
-    merchantId = serializers.CharField()
-    normalReturnUrl = serializers.URLField()
-    orderChannel = serializers.CharField()
-    transactionReference = serializers.IntegerField()
-    Seal = serializers.CharField(min_length = 1)
+    class Meta:
+        model = TransactionMercanet
+        fields = ('amount', 'currencyCode', 'interfaceVersion', 'keyVersion', 'merchantID', 'normalReturnUrl', 'transactionReference', 'Seal')
 
 # comment peut-on ajouter le seal à ce serializer ? il faut que ce field (qui représente la requête à Mercanet), contienne le hash du seal
