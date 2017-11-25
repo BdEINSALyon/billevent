12e5650ab3bbcd9d0792eaecfb9c03576f77d5f1
Jean Ribes <jean.christophe.ribes@sfr.fr>
2017-11-25 19:00:34 +0100

    rajout d'un message d'erreur si rien n'est donnée en argument

--- mercanet/urls.py
+++ mercanet/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     url(r'^([0-9]+)$', views.MercanetViewSet.pay),
-    url(r'^.+$', views.MercanetViewSet.error)
+    url(r'^([0-9]+)/check/$', views.MercanetViewSet.check),
+    url(r'^auto/(.+)$', views.MercanetViewSet.autoMercanet),
+    url(r'^.+$', views.MercanetViewSet.error),
+    url(r'^$', views.MercanetViewSet.error)
 ]
