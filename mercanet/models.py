3349154b4632720fa85ae220ef9116ebc69769bf
Jean Ribes <jean.christophe.ribes@sfr.fr>
2017-11-26 03:49:52 +0100

    sauf id & montant, tout peut être vide & nul c'est peut être moche & inutile mais ça marche

--- mercanet/models.py
+++ mercanet/models.py
@@ -1,28 +1,29 @@
 from django.db import models
-import os
-import sealTransaction
+from django.utils.translation import ugettext_lazy as _
 # Create your models here.
 
 class TransactionMercanet(models.Model):
     class Meta:
-        verbose_name = _('Transaction')
-        transactionReference =models.IntegerField(verbose_name=_("id de la transaction, le même que MercaNET"))
-        amount = models.IntegerField(verbose_name=_("montant payé"))
-        maskedPan = models.CharField(verbose_name=_("numéro de CB masqué", max_length=254)
-        cardProductName = models.CharField(max_length=100, verbose_name=_("nom de la CB"))
-        panExpiryDate = models.IntegerField(max_length=50, verbose_name=_("date d'expiration ?"))
-        customerIpAddress = models.IPAddressField(verbose_name=_('On a le droit de le stocker ?'))
-        complementaryCode = models.IntegerField(verbose_name=_("C koi ?"))
-        panEntryMode = models.CharField(verbose_name=_("mode d'entrée du n° de la CB"))
-        captureDay = models.IntegerField()
-        responseCode = models.IntegerField(max_length=10, verbose_name=_("utile ?"))
-        issuerCountryCode = models.CharField(verbose_name=_("Pays émetteur de la CB"), max_length="5")
-        customerMobilePhone = models.CharField(verbose_name=_("numéro de téléphone"), max_length="30")
-        paymentMeanBrand = models.CharField(verbose_name=_("organisme de paiement ex: VISA"))
-        issuerCode = models.IntegerField(verbose_name=_("C koi ?"))
-        transactionDateTime = models.DateTimeCheckMixin(verbose_name=_("Date de la transaction"))
-        paymentMeanType = models.CharField(verbose_name=_("type de paiement ex: CB, PayPal..."))
-        captureLimitDate = models.IntegerField(verbose_name=_("C koi ? temps pour annuler le paiement ?"))
-        cardProductCode = models.CharField(verbose_name=_("important ?"))
+        verbose_name = _('Transaction avec la BNP')
+    transactionReference = models.IntegerField(verbose_name=_('id de la transaction, le même que MercaNET'))
+    amount = models.IntegerField(verbose_name=_("montant payé"))
+    maskedPan = models.CharField(verbose_name=_("numéro de CB masqué"), max_length=254, blank=True)
+    cardProductName = models.CharField(max_length=100, verbose_name=_("nom de la CB"), blank=True)
+    panExpiryDate = models.IntegerField(verbose_name=_("date d'expiration ?"), blank=True, null=True)
+    customerIpAddress = models.GenericIPAddressField(verbose_name=_('On a le droit de le stocker ?'), blank=True, null=True)
+    complementaryCode = models.IntegerField(verbose_name=_("C koi ?"), blank=True, null=True)
+    panEntryMode = models.CharField(max_length=20,verbose_name=_("mode d'entrée du n° de la CB"), blank=True, null=True)
+    captureDay = models.IntegerField(blank=True, null=True)
+    responseCode = models.CharField(max_length=2, verbose_name=_("(responseCode)VERIF PAIEMENT SI=0"), blank=True, null=True)
+    issuerCountryCode = models.CharField(verbose_name=_("Pays émetteur de la CB"), max_length=5, blank=True, null=True)
+    customerMobilePhone = models.CharField(verbose_name=_("numéro de téléphone"), max_length=30, blank=True, null=True)
+    paymentMeanBrand = models.CharField(max_length=20,verbose_name=_("organisme de paiement ex: VISA"), blank=True, null=True)
+    issuerCode = models.IntegerField(verbose_name=_("C koi ?"), blank=True, null=True)
+    transactionDateTime = models.DateTimeField(verbose_name=_("Date de la transaction"), blank=True, null=True)
+    paymentMeanType = models.CharField(max_length=20,verbose_name=_("type de paiement ex: CB, PayPal..."), blank=True, null=True)
+    captureLimitDate = models.IntegerField(verbose_name=_("C koi ? temps pour annuler le paiement ?"), blank=True, null=True)
+    cardProductCode = models.CharField(max_length=5,verbose_name=_("important ?"), blank=True, null=True)
+    def __str__(self):
+        return str(self.transactionReference)
 
 
