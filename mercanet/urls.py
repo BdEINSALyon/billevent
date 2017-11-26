ec9587719dc5b38012d9dc4f13cc4cda80496472
Jean Ribes <jean.christophe.ribes@sfr.fr>
2017-11-26 03:52:02 +0100

    modifié le fonctionnement des url, maitenant c'est /pay/check/id

--- mercanet/urls.py
+++ mercanet/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^([0-9]+)$', views.MercanetViewSet.pay),
-    url(r'^([0-9]+)/check/$', views.MercanetViewSet.check),
+    url(r'^check/([0-9]+)$', views.MercanetViewSet.check),
     url(r'^auto/(.+)$', views.MercanetViewSet.autoMercanet),
     url(r'^.+$', views.MercanetViewSet.error),
     url(r'^$', views.MercanetViewSet.error)
