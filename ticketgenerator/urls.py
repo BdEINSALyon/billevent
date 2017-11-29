12528db2db3ae3e9aa0b8d7e8a756431b3bf6c66
François Lallevé <francois.lalleve@laposte.net>
2017-11-29 19:02:45 +0100

    Work better and more security !

--- ticketgenerator/urls.py
+++ ticketgenerator/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
-from . import views
+from . import views, getid
 
 urlpatterns = [
-    url(r'^(?P<id>[0-9A-Za-z-:]+)/print', views.generate_ticket, name='ticket-print'),
+    url(r'^(?P<id>[0-9A-Za-z-:_]+)/print', views.generate_ticket, name='ticket-print'),
 ]
