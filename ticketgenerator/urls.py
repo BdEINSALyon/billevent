55b453e6cc23eeb5d462ce072f04229320a16baf
François Lallevé <francois.lalleve@laposte.net>
2017-11-29 19:04:13 +0100

    Oups

--- ticketgenerator/urls.py
+++ ticketgenerator/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from . import views, getid
+from . import views
 
 urlpatterns = [
     url(r'^(?P<id>[0-9A-Za-z-:_]+)/print', views.generate_ticket, name='ticket-print'),
