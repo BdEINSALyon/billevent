9a331074c81505b3617eae3f912f37465cb8736c
Nornort <francois.lalleve@laposte.net>
2017-11-29 19:09:06 +0100

    Delete getid.py

--- ticketgenerator/getid.py
+++
@@ -1,9 +0,0 @@
-from django.core.signing import TimestampSigner
-from django.http import HttpResponse
-import datetime
-
-def getid(request, id):
-    now = datetime.datetime.now()
-    uid = TimestampSigner().sign(id)
-    html = "<html><body>ID : %s à </body></html>" % uid + "<html><body>%s</body></html>" % now
-    return HttpResponse(html)
