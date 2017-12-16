48f43926f384f6a74f96c70acc4073214642a3ca
Philippe Vienne <philippegeek@gmail.com>
2017-12-16 15:26:27 +0100

    Add uploads management

--- billetterie/settings.py
+++ billetterie/settings.py
@@ -163,7 +163,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/uploads/'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+MEDIA_ROOT = os.path.join(BASE_DIR, "uploads")
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
